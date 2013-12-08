ffb2f3607b3142364f75d19a9c94970e1a154531
jphollanti <pikkuaapo@gmail.com>
2013-12-08 12:03:57 +0200

    Plumbing

---
+++ routes/invoice.js
@@ -0,0 +1,17 @@
+
+exports.customer = function(req, res) {
+  res.send("customers list");
+};
+
+exports.person = function(req, res) {
+  res.send("person list");
+};
+
+exports.invoice = function(req, res) {
+  res.send("invoice list");
+};
+
+exports.product = function(req, res) {
+  res.send("product list");
+};
+
